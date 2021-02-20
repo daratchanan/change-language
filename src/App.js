85a8804b166c845e7a48d34fd17be0ac26540a31
jarabeenoi <nongnat61@gmail.com>
2021-02-20 19:27:57 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, MenuItem, Select } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const { t, i18n } = useTranslation();
+   const handleClick = (la) => {
+      i18n.changeLanguage(la);
+   };
+
+   return (
+      <div className="App">
+         <nav style={{ width: "100%", padding: "2rem 0", backgroundColor: "gray" }}>
+
+            {/* <Select
+               labelId="demo-simple-select-label"
+               id="demo-simple-select"
+               onChange={(e) => handleClick(e.target.value)}
+            >
+               <MenuItem value={"en"}>English</MenuItem>
+               <MenuItem value={"th"}>Thai</MenuItem>
+            </Select> */}
+            <Button variant="contained" color="primary" onClick={() => handleClick("en")}>English</Button>
+            <Button variant="contained" color="secondary" onClick={() => handleClick("th")}>Thai</Button>
+         </nav>
+         <p>{t("Hello")}</p>
+         <p>{t("Goobye")}</p>
+
+      </div>
+   );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./i18next";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <Suspense fallback={(<div>Loading ~~~~</div>)}>
+         <App />
+      </Suspense>
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
